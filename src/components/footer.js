ac1799c8a5a7a1de6a13fef7abd69155ee839b3c
Tommy Treb <tomtrezb2003@gmail.com>
2020-03-05 00:14:17 -0600

    Added socials

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,4 +1,6 @@
 import React from "react"
+import { FaFacebookF, FaInstagram } from "react-icons/fa"
+import { IconButton } from "@material-ui/core"
 
 export default () => {
   return (
@@ -9,9 +11,29 @@ export default () => {
         background: `#580D7D`,
         color: "white",
         padding: 12,
+        justifyContent: "space-between",
+        alignItems: "center",
       }}
     >
-      © {new Date().getFullYear()}, Stevens Point Ignite Young Professionals
+      <span>
+        © {new Date().getFullYear()}, Stevens Point Ignite Young Professionals
+      </span>
+      <div>
+        <IconButton
+          href={"https://www.facebook.com/IgnitePortageCountyWI/"}
+          color="secondary"
+        >
+          <FaFacebookF />
+        </IconButton>
+        <IconButton
+          href={
+            "https://www.instagram.com/explore/locations/315407191810577/ignite-young-professionals/"
+          }
+          color="secondary"
+        >
+          <FaInstagram />
+        </IconButton>
+      </div>
     </footer>
   )
 }
